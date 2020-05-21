a44c5fe8265eaa72ac4d063bde354443b39d71d0
cesar-machado <cesarmach19@gmail.com>
2020-05-21 18:19:50 -0300

    Coloquei animaçao e falta arrumar o js

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
-
 const navSlide = () => {
     const burguer = document.querySelector('.burguer');
     const nav = document.querySelector('.links');
 
+
+
     burguer.addEventListener('click', () => {
         nav.classList.toggle('nav-active');
+    });
+
+    nav.addEventListener('click', () => {
+        nav.classList.toggle('nav-disable')
     })
+
 }
 
 navSlide();
